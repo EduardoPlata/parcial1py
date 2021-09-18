b21ef9ba29fb3db69784ea1a59f8e6878e618a4d
Eduardo Plata <eduardplatt77@gmail.com>
2021-09-17 20:56:27 -0500

    Actualización información final

--- parcial1.py
+++ parcial1.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import hashlib
+import re
 
 db = mysql.connector.connect(
     host='localhost',
@@ -22,24 +23,6 @@ def registrarUsuario(usuario):
     ''' cursor.execute('        insert into usuarios(nombre,email,contrasena)        values (%s,%s,%s)',        (dato)) '''
     db.commit()
     cursor.close()
-
-
-def crearUsuario(nombre, email, contrasena):
-    cursor = db.cursor()
-
-    cursor.execute('''inser into
-        usuarios(nombre, email, contrasena)
-        values(%s, %s, %s)''', (
-            nombre,
-            email, contrasena
-            ))
-
-    #Creacion, modificaion, eliminacio de datos
-    # => db.commit()
-
-    cursor.close()
-
-
 
 
 cursor = db.cursor()
@@ -69,7 +52,7 @@ while opcion !=0:
         contrasena = input('Ingrese contraseña, mínimo 8 caracteres, una mayúscula y un carácter especial: ')
         ')
 
-        #.append es para agresar en lo ultimo de la lista
+     
         contactos.append({
             'nombre': nombre,
             'apellido': apellido,
@@ -77,24 +60,32 @@ while opcion !=0:
             'contrasena': contrasena,
 
         })
-
         input('Usuario guardado correctamente. Presione enter para continuar')
+
     elif opcion ==2:
         correo = input('Ingrese el correo del usuario: ')
-        contrasena = input('Ingrese contraseña: ')
-        print(usuarios)
+
+        correos = [correo]
+ 
+        for correo in correos:
+        print("Probando si '{}' es válido...{}".format(correo, es_correo_valido(correo)))
 
 
-        h = hashlib.new("sha512",b"Unipython")
+        contrasena = input('Ingrese contraseña: ')        
+
+        def validarContrasena(contrasena):
+            if 8 <= len(contrasena) <= 16:
+                if re.search('[a-z]', contrasena) and re.search('[A-Z]', contrasena):
+                    if re.search('[0-9]', contrasena):
+                        if re.search('[$@#]', contrasena):
+                             return True    
+            return False        
+
+        print(validarContrasena(clave))
+      
+
 
     
 
 
 
-
-CREATE TABLE usuarios(  
-    id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
-    nombre VARCHAR(255) not NULL,
-    email VARCHAR(255) NOT NULL,
-    contrasenaa VARCHAR(50) not NULL
-) ;
